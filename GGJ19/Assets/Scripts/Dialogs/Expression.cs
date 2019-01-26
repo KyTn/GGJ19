641684f524bec542503e104a2679bd9ff4f97284
jesmartinez <axxel.guijarrock@hotmail.es>
2019-01-26 10:27:03 +0100

    DialogScript Update 1

--- GGJ19/Assets/Scripts/Dialogs/Expression.cs
+++ GGJ19/Assets/Scripts/Dialogs/Expression.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Expresion : MonoBehaviour
+public class Expression : MonoBehaviour
 {
     public int id;
     public string iconName;
