0b7a020fd11e67bb2edac6f3ec99796aa8c633e6
KyTn <angel.kytn@gmail.com>
2019-01-26 22:04:40 +0100

    dialogggggggggs

--- GGJ19/Assets/Scripts/Dialogs/DialogObject.cs
+++ GGJ19/Assets/Scripts/Dialogs/DialogObject.cs
@@ -1,8 +1,39 @@
 ﻿using System.Collections.Generic;
 
-public class DialogObject
+
+public enum DialogType
 {
-    List<SymbolId> SymbolsNiceQuery;
-    List<SymbolId> SymbolsNiceResponse;
-    List<SymbolId> SymbolsBadResponse;
+    DialogFinalObject, InterDialogObject, NoDialogObject
+}
+
+public abstract class DialogObject
+{
+    public abstract DialogType DialogType { get; }
+    public List<SymbolId> OtherSymbolsDialog;
+}
+
+
+public class DialogFinalObject : DialogObject
+{
+    public List<SymbolId> PlayerSymbolsNiceDialog;
+    public List<SymbolId> OtherSymbolsNiceResponse;
+    public List<SymbolId> OtherSymbolsBadResponse;
+
+    public override DialogType DialogType => DialogType.DialogFinalObject;
+}
+
+
+public class InterDialogObject : DialogObject
+{
+    public List<SymbolId> PlayerSymbolsNiceDialog;
+    public int OtherDialogNiceResponse;
+    public int OtherDialogBadResponse;
+
+    public override DialogType DialogType => DialogType.InterDialogObject;
+}
+
+public class NoDialogObject : DialogObject
+{
+
+    public override DialogType DialogType => DialogType.NoDialogObject;
 }
--- GGJ19/Assets/Scripts/Dialogs/NPCDialog.cs
+++ GGJ19/Assets/Scripts/Dialogs/NPCDialog.cs
@@ -16,6 +16,6 @@ public class NormalNPCDialog : NPCDialog
 
 public class ConversationDialog : NPCDialog
 {
-
+     
 }
 
