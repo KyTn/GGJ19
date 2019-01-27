a338e7e3e6b156e19d4ea744179bc83f6ce1cab3
KyTn <angel.kytn@gmail.com>
2019-01-27 14:29:29 +0100

    sounds

---
+++ GGJ19/Assets/Scripts/Utilities/AutoStartMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class AutoStartMusic : MonoBehaviour
+{
+    public AudioClip music;
+
+    public AudioSource asource;
+
+    private void Start()
+    {
+        asource.clip = music;
+
+        asource.loop = true;
+        asource.Play();
+    }
+
+}
